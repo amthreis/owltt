17f924a38df055a1c1f8cb3572f250e552a71a63
amthreis <amthreis@gmail.com>
2024-05-25 02:03:56 -0300

    feat: basic scrollcontainer for the graph

---
+++ Code/UI/UIGraphHour.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    [Tool]
+    public partial class UIGraphHour : Control
+    {
+
+    }
+}
