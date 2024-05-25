c5dc960cac0c1e70eef97018818fb432224b6b09
amthreis <amthreis@gmail.com>
2024-05-25 15:52:02 -0300

    feat: change lines and hours dinamically based on targetHours

---
+++ Code/Main.cs
@@ -0,0 +1,42 @@
+﻿using Godot;
+
+namespace Task
+{
+    [Tool]
+    public partial class Main : Node
+    {
+        int _targetHours;
+
+        [Export(PropertyHint.Range, "1, 12")]
+        public int TargetHours
+        {
+            get => _targetHours;
+            set
+            {
+                _targetHours = value;
+
+                UpdateHoursAndLines();
+            }
+        }
+
+        [Export] VBoxContainer linesCtr;
+        [Export] VBoxContainer hoursCtr;
+
+        void UpdateHoursAndLines()
+        {
+            if (hoursCtr == null || linesCtr == null)
+                return;
+
+            //GD.Print(chd.Name);
+
+            for (var i = 0; i < 12; i++)
+            {
+                var hChd = hoursCtr.GetChild<Control>(hoursCtr.GetChildCount() - 2 - i);
+                var lChd = linesCtr.GetChild<Control>(linesCtr.GetChildCount() - 1 - i);
+
+                hChd.Visible = i < _targetHours;
+                lChd.Visible = i < _targetHours;
+            }
+        }
+    }
+}
---
+++ Code/UI/UIGraphItem.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    [Tool]
+    public partial class UIGraphItem : VBoxContainer
+    {
+        float _value;
+
+        [Export(PropertyHint.Range, "0, 1")]
+        public float Value 
+        { 
+            get { return _value; }
+            set {  
+                _value = value;
+
+                GetNode<Control>("Remainder").SizeFlagsStretchRatio = 1f - _value;
+                GetNode<Panel>("Panel").SizeFlagsStretchRatio = _value;
+            }
+        }
+    }
+}
